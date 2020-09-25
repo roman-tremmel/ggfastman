5f978e9cdf2510dbb1a108bee1f79d860b50b527
Tremmel <tremmel@rbk.intra>
2020-09-25 11:37:54 +0200

    update

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,43 @@
+get_chrom_lengths=function(build=c('hg18','hg19','hg38')){
+  build=match.arg(build)
+  
+  chrom_lengths_hg18=c(chr1=247249719,chr2=242951149,chr3=199501827,
+                       chr4=191273063,chr5=180857866,chr6=170899992,
+                       chr7=158821424,chr8=146274826,chr9=140273252,
+                       chr10=135374737,chr11=134452384,chr12=132349534,
+                       chr13=114142980,chr14=106368585,chr15=100338915,
+                       chr16=88827254,chr17=78774742,chr18=76117153,
+                       chr19=63811651,chr20=62435964,chr21=46944323,
+                       chr22=49691432)
+  
+  
+  chrom_lengths_hg19=c(chr1=249250621,chr2=243199373,chr3=198022430,
+                       chr4=191154276,chr5=180915260,chr6=171115067,
+                       chr7=159138663,chr8=146364022,chr9=141213431,
+                       chr10=135534747,chr11=135006516,chr12=133851895,
+                       chr13=115169878,chr14=107349540,chr15=102531392,
+                       chr16=90354753,chr17=81195210,chr18=78077248,
+                       chr19=59128983,chr20=63025520,chr21=48129895,
+                       chr22=51304566)
+  
+  # https://genome.ucsc.edu/goldenpath/help/hg38.chrom.sizes
+  chrom_lengths_hg38=c(chr1=248956422,chr2=242193529,chr3=198295559,
+                       chr4=190214555,chr5=181538259,chr6=170805979,
+                       chr7=159345973,chr8=145138636,chr9=138394717,
+                       chr10=133797422,chr11=135086622,chr12=133275309,
+                       chr13=114364328,chr14=107043718,chr15=101991189,
+                       chr16=90338345,chr17=83257441,chr18=80373285,
+                       chr19=58617616,chr20=64444167,chr21=46709983,
+                       chr22=50818468)
+  
+  if (build=='hg18'){
+    return(chrom_lengths_hg18)
+  } else if (build=='hg19'){
+    return(chrom_lengths_hg19)
+  } else {
+    return(chrom_lengths_hg38)
+  }
+}
 get_cumulative_length=function(chrom_lengths){
   n_chrom=length(chrom_lengths)
   cumulative_length=c(0,cumsum(x = unname(chrom_lengths))[1:(n_chrom-1)])
@@ -21,14 +61,14 @@ get_x_breaks=function(chrom_lengths){
 add_cumulative_pos=function(data,build=c('hg18','hg19','hg38')){
   build=match.arg(build)
 
-  if (!all(c('chrom','pos','y')%in%colnames(data))){
-    stop('data must have chrom, pos and y columns')
+  if (!all(c('chr','pos','y')%in%colnames(data))){
+    stop('data must have chr, pos and y columns')
   }
 
   chrom_lengths=get_chrom_lengths(build)
   cumulative_length=get_cumulative_length(chrom_lengths)
 for (i in paste0('chr',seq(1,22))){
-    data[data$chrom==i,'cumulative_pos']=data[data$chrom==i,'pos']+cumulative_length[i]
+    data[data$chr==i,'cumulative_pos']=data[data$chr==i,'pos']+cumulative_length[i]
   }
 
   return(data)
@@ -41,7 +81,7 @@ add_color=function(data,color1='black',color2='grey'){
     user_color=rep(NA,nrow(data))
   }
 
-  data$color=ifelse(data$chrom%in%paste0('chr',seq(1,22,2)),color1,color2)
+  data$color=ifelse(data$chr%in%paste0('chr',seq(1,22,2)),color1,color2)
   data$color=ifelse(is.na(user_color),data$color,user_color)
   return(data)
 }
@@ -66,3 +106,4 @@ add_fill = function(data){
   }
   return(data)
 }
+
